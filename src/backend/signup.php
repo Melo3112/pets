1b42c5a72f34a680331c2bf653bcb52d019ee8b5
Melo3112 <marlonandres20@gmail.com>
2024-04-29 17:09:18 -0500

    envio

---
+++ src/backend/signup.php
@@ -0,0 +1,12 @@
+<?php
+ $fullname = $_POST['fname'];
+ $email = $_POST['email'];
+ $password = $_POST['password'];
+ $enc_pass = md5($password);
+
+ echo "Your fullname: ".$fullname."<br>";
+ echo "Your email: ".$email."<br>";
+ echo "Your password: ".$password."<br>";
+ echo "Your password encri: ".$enc_pass."<br>";
+
+?>
