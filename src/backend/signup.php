69860ed7a4e6f5e0b11fc80b828e306451653b95
Melo3112 <marlonandres20@gmail.com>
2024-04-29 17:36:18 -0500

    database pets

--- src/backend/signup.php
+++ src/backend/signup.php
@@ -1,12 +1,24 @@
 <?php
- $fullname = $_POST['fname'];
- $email = $_POST['email'];
- $password = $_POST['password'];
- $enc_pass = md5($password);
+include('../../config/database.php');
 
- echo "Your fullname: ".$fullname."<br>";
- echo "Your email: ".$email."<br>";
- echo "Your password: ".$password."<br>";
- echo "Your password encri: ".$enc_pass."<br>";
+    $fullname = $_POST['fname'];
+    $email = $_POST['email'];
+    $passwd = $_POST['passwd'];
+    $enc_pass = md5($passwd);
+
+    $sql = "
+        INSERT INTO users (fullname, email, password) 
+            values('$fullname', '$email', '$enc_pass')
+    ";
+
+    $ans = pg_query($conn,$sql);
+    if ($ans){
+        echo "User has been created succesfully";
+    }else{
+        echo "Error". pg_last_error();
+    }
+
+    //close connection
+    pg_close($conn)
 
 ?>
