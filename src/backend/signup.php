f1750bfffbf295b1f18af67726e068ce60348f7d
Melo3112 <marlonandres20@gmail.com>
2024-05-03 12:30:19 -0500

    Corte 2

--- src/backend/signup.php
+++ src/backend/signup.php
@@ -6,19 +6,33 @@ include('../../config/database.php');
     $passwd = $_POST['passwd'];
     $enc_pass = md5($passwd);
 
-    $sql = "
+    $sql_validate_email = "SELECT * FROM users WHERE email ='$email'";
+    $result = pg_query($conn, $sql_validate_email);
+    $total = pg_num_rows($result);
+
+    if($total>0){
+        echo"<script>alert('Email alredy exists')</script>";
+        header("refresh:0;url=../Signin.html");
+    }else{
+        $sql = "
         INSERT INTO users (fullname, email, password) 
             values('$fullname', '$email', '$enc_pass')
     ";
 
     $ans = pg_query($conn,$sql);
     if ($ans){
+        header("refresh:0;url=../Signin.html");
         echo "User has been created succesfully";
     }else{
         echo "Error". pg_last_error();
     }
 
-    //close connection
-    pg_close($conn)
+    
 
+
+    }
+
+    pg_close($conn) 
+
+    
 ?>
