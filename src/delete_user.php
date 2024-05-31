3d7d29bfc3ebf09f4c2ce30f8aa83c9563d6ea2f
Melo3112 <marlonandres20@gmail.com>
2024-05-31 11:35:12 -0500

    examen

--- src/delete_user.php
+++ src/delete_user.php
@@ -7,13 +7,6 @@ $id = $_GET['id'];
 $query_delete = "DELETE FROM users WHERE id = $id";
 $result_delete = pg_query($conn, $query_delete);
 
-if ($result_delete) {
-    echo "Usuario eliminado correctamente";
-} else {
-    echo "Error al eliminar el usuario: " . pg_last_error($conn);
-}
-
-
 header("Location: list_users.php");
 exit;
 ?>
